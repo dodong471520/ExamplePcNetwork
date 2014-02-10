ee372408b37beb38a7faebe33c4310255b5753c0
dodong471520 <1012131075@qq.com>
2014-02-10 19:06:08 +0800

    init

---
+++ TestPcNetwork/TestPcNetwork/g_platform.cpp
@@ -0,0 +1,9 @@
+#include "g_platform.h"
+
+void Sys_Log( char *format,... )
+{
+	va_list args;
+	va_start(args,format);
+	char buff[256];
+	vs
+}
---
+++ TestPcNetwork/TestPcNetwork/g_platform.h
@@ -0,0 +1,9 @@
+#ifndef __G_PLATFORM_H__
+#define __G_PLATFORM_H__
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void Sys_Log(char *format,...);
+
+#endif
